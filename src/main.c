c30f3bd41cb9da4d856b4f653bb13436df1ff0a4
Degamisu <degamisu@gmail.com>
2023-11-22 17:39:58 -0500

    add SDK

--- src/main.c
+++ src/main.c
@@ -1,17 +1,81 @@
-/**
- * Hello World Example
- * Created With Genesis-Code extension for Visual Studio Code
- * Use "Genesis Code: Compile" command to compile this program.
- **/
 #include <genesis.h>
+
+// Player position
+int playerX = 160;
+int playerY = 120;
+
+// Player speed
+int speed = 2;
+
+// Animation variables
+int frame = 0;
+int frameCounter = 0;
+
+void updatePlayer()
+{
+    // Read input
+    u16 value = JOY_readJoypad(JOY_1);
+
+    // Move player
+    if (value & BUTTON_UP)
+    {
+        playerY -= speed;
+    }
+    if (value & BUTTON_DOWN)
+    {
+        playerY += speed;
+    }
+    if (value & BUTTON_LEFT)
+    {
+        playerX -= speed;
+    }
+    if (value & BUTTON_RIGHT)
+    {
+        playerX += speed;
+    }
+
+    // Update animation
+    frameCounter++;
+    if (frameCounter == 10)
+    {
+        frame++;
+        if (frame > 2)
+        {
+            frame = 0;
+        }
+        frameCounter = 0;
+    }
+}
+
+void drawPlayer()
+{
+    // Draw player character "@" with animation
+    VDP_drawText("   ", playerX - 8, playerY - 8);
+    VDP_drawText(" @ ", playerX - 8, playerY);
+    VDP_drawText("   ", playerX - 8, playerY + 8);
+}
 
 int main()
 {
-    VDP_drawText("Hello Sega!!", 10,13);
-    while(1)
+    // Initialize SGDK
+    VDP_init();
+    JOY_init();
+
+    // Main loop
+    while (1)
     {
-        //For versions prior to SGDK 1.60 use VDP_waitVSync instead.
-        SYS_doVBlankProcess();
+        // Clear the screen
+        VDP_clearPlan(APLAN);
+
+        // Update and draw the player
+        updatePlayer();
+        drawPlayer();
+
+        // Wait for VBlank before flipping the screen
+        VDP_waitVSync();
+        VDP_showFPS(FALSE);
+        VDP_flip();
     }
-    return (0);
+
+    return 0;
 }
